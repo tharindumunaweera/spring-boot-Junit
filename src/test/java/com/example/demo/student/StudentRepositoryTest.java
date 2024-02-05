302bab61c402a3316bd89650005d63c92f8d14c8
tharindumunaweera <tharindumunaweera94@gmail.com>
2024-02-05 16:41:22 +0530

    test add student method using mockito

--- src/test/java/com/example/demo/student/StudentRepositoryTest.java
+++ src/test/java/com/example/demo/student/StudentRepositoryTest.java
@@ -1,15 +1,28 @@
 package com.example.demo.student;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-
+@DataJpaTest
 class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository underTest;
+
+    @AfterEach
+    void tearDown() {
+
+    }
+
+    @BeforeEach
+    void setUp() {
+        underTest.deleteAll();
+    }
 
     @Test
     void selectExistsEmail() {
@@ -22,5 +35,18 @@ class StudentRepositoryTest {
         Boolean  exists = underTest.selectExistsEmail(email);
 
         assertThat(exists).isTrue();
+    }
+
+    @Test
+    void selectedEmailNotExists() {
+
+        String email = "jamila1@gmail.com";
+        Student student = new Student("jamila", "jamila@gmail.com", Gender.FEMALE);
+
+        underTest.save(student);
+
+        Boolean  exists = underTest.selectExistsEmail(email);
+
+        assertThat(exists).isFalse();
     }
 }
