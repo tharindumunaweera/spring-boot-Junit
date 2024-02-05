56c9dc56e7574aa4bbc0a5287fd06eac7eee9a4f
tharindumunaweera <tharindumunaweera94@gmail.com>
2024-02-05 17:37:03 +0530

    implement unit test for saving student when email is exist

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -1,14 +1,19 @@
 package com.example.demo.student;
 
+import com.example.demo.student.exception.BadRequestException;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -34,8 +39,29 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addStudent() {
+    void canAddStudent() {
+        Student student = new Student("jamila", "jamila@gmail.com", Gender.FEMALE);
+
+        underTest.addStudent(student);
+
+        ArgumentCaptor<Student> studentArgumentCaptor = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository).save(studentArgumentCaptor.capture());
+
+        Student captureStudent = studentArgumentCaptor.getValue();
+
+        assertThat(captureStudent).isEqualTo(student);
+
+    }
+
+    @Test
+    void canAddExistingStudent() {
+        Student student = new Student("jamila", "jamila@gmail.com", Gender.FEMALE);
+
+        given(studentRepository.selectExistsEmail(student.getEmail())).willReturn(true);
+
+        assertThatThrownBy(() -> underTest.addStudent(student)).isInstanceOf(BadRequestException.class)
+                .hasMessageContaining("Email " + student.getEmail() + " taken");
     }
 
     @Test
